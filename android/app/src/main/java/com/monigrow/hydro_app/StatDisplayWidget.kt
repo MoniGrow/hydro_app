6901e6d112df46ea29425c5d64c0a4c82e7a6ad3
Jerry Yan <jeyan@ucsd.edu>
2021-04-27 18:20:49 -0700

    Add tap to widget, update widget on signin/out events

--- android/app/src/main/java/com/monigrow/hydro_app/StatDisplayWidget.kt
+++ android/app/src/main/java/com/monigrow/hydro_app/StatDisplayWidget.kt
@@ -1,13 +1,16 @@
 package com.monigrow.hydro_app
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ActivityNotFoundException
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -21,6 +24,7 @@ import io.flutter.plugins.firebase.auth.Constants.TAG
 
 /**
  * todo let user select the data type (or just do all of them)
+ * todo open the app when touched
  */
 class StatDisplayWidget : AppWidgetProvider() {
     private lateinit var db: FirebaseFirestore
@@ -34,7 +38,26 @@ class StatDisplayWidget : AppWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
+            addAppLaunchIntent(context, appWidgetManager, appWidgetId)
             updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    private fun addAppLaunchIntent(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+        try {
+            val intent = Intent("android.intent.action.MAIN")
+            intent.addCategory("android.intent.category.LAUNCHER")
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            intent.component = ComponentName(context.packageName, MainActivity::class.java.name)
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                    context, 0, intent, 0)
+            val views = RemoteViews(context.packageName, R.layout.stat_display_widget)
+            views.setOnClickPendingIntent(R.id.app_button, pendingIntent)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(context.applicationContext,
+                    "There was a problem loading the application: ",
+                    Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -47,6 +70,10 @@ class StatDisplayWidget : AppWidgetProvider() {
             // should probably change so that text on widget shows no user anymore, but
             // it's whatever
             Log.d(TAG, "USER IS NULL, NOT FOUND LLLLLLLLLLLLLLLLLLLLLLLLLLLL")
+            rv.setTextViewText(R.id.waterlevel_text, "No user or data")
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, rv)
+            return
         }
         auth.currentUser?.let { user ->
             Log.d(TAG, "User found")
