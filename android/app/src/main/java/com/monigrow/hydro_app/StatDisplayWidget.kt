4952c222420a11f5883bd609930b5a1c38c11042
Jerry Yan <jeyan@ucsd.edu>
2021-05-20 00:57:35 -0700

    Android app may or may not be functional

--- android/app/src/main/java/com/monigrow/hydro_app/StatDisplayWidget.kt
+++ android/app/src/main/java/com/monigrow/hydro_app/StatDisplayWidget.kt
@@ -13,21 +13,20 @@ import android.widget.RemoteViewsService
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreException
-import com.google.firebase.firestore.Query
-import com.google.firebase.firestore.QuerySnapshot
-import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 import io.flutter.plugins.firebase.auth.Constants.TAG
 
+
+@IgnoreExtraProperties
+data class Temperature(val temperature: Double? = null, val timestamp: Double? = null) {
+}
 
 /**
  * todo let user select the data type (or just do all of them)
  * todo open the app when touched
  */
 class StatDisplayWidget : AppWidgetProvider() {
-    private lateinit var db: FirebaseFirestore
     private lateinit var auth: FirebaseAuth
     private var currentValue: String = "None"
 
@@ -61,16 +60,49 @@ class StatDisplayWidget : AppWidgetProvider() {
         }
     }
 
+    private fun generateListener(field: String, uid: String, db: FirebaseDatabase, rv: RemoteViews, appWidgetManager: AppWidgetManager, appWidgetId: Int, textId: Int) {
+        val ref = db.getReference("users/$uid/sensor_data/$field")
+        ref.orderByChild("timestamp").limitToLast(1).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    // unchecked cast because i dont care enough
+                    val data: HashMap<String, HashMap<String, Any>> = snapshot.value as HashMap<String, HashMap<String, Any>>
+                    Log.d(TAG, "Current data: $data")
+                    Log.d(TAG, "type: ${data::class.qualifiedName}")
+                    for (x in data) {
+                        if (x.component2()[field] is Long || x.component2()[field] is Int) {
+                            currentValue = x.component2()[field].toString()
+                        } else {
+                            currentValue = "%.2f".format(x.component2()[field])
+                        }
+                    }
+                    // todo: only update when new data is in?
+                    rv.setTextViewText(textId, "$field: $currentValue")
+                    // Instruct the widget manager to update the widget
+                    appWidgetManager.updateAppWidget(appWidgetId, rv)
+                } else {
+                    Log.d(TAG, "No data in snapshot.")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+    }
+
     private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
         Log.d(TAG, "updating widget")
         val rv = RemoteViews(context.packageName, R.layout.stat_display_widget)
-        db = Firebase.firestore
+        val db = FirebaseDatabase.getInstance()
         auth = Firebase.auth
         if (auth.currentUser == null) {
             // should probably change so that text on widget shows no user anymore, but
             // it's whatever
             Log.d(TAG, "USER IS NULL, NOT FOUND LLLLLLLLLLLLLLLLLLLLLLLLLLLL")
             rv.setTextViewText(R.id.waterlevel_text, "No user or data")
+            rv.setTextViewText(R.id.humidity_text, "")
+            rv.setTextViewText(R.id.temp_text, "")
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, rv)
             return
@@ -78,19 +110,9 @@ class StatDisplayWidget : AppWidgetProvider() {
         auth.currentUser?.let { user ->
             Log.d(TAG, "User found")
             val uid = user.uid
-            val docRef = db.collection("ESP32data").document(uid).collection("sensorData")
-            docRef.orderBy("timestamp", Query.Direction.DESCENDING).limit(1).get().addOnSuccessListener { snapshot ->
-                if (snapshot != null && !snapshot.isEmpty) {
-                    Log.d(TAG, "Current data: ${snapshot.first().data}")
-                    currentValue = snapshot.first().data["temperature"].toString()
-                    // todo: only update when new data is in?
-                    rv.setTextViewText(R.id.waterlevel_text, "temperature: $currentValue")
-                    // Instruct the widget manager to update the widget
-                    appWidgetManager.updateAppWidget(appWidgetId, rv)
-                } else {
-                    Log.d(TAG, "No data in snapshot.")
-                }
-            }
+            generateListener("temperature", uid, db, rv, appWidgetManager, appWidgetId, R.id.temp_text)
+            generateListener("humidity", uid, db, rv, appWidgetManager, appWidgetId, R.id.humidity_text)
+            generateListener("water_level", uid, db, rv, appWidgetManager, appWidgetId, R.id.waterlevel_text)
         }
     }
 
@@ -111,83 +133,5 @@ class StatDisplayWidget : AppWidgetProvider() {
             val rv = RemoteViews(context.packageName, R.layout.stat_display_widget)
             rv.removeAllViews(appWidgetId)
         }
-    }
-}
-
-// doesn't work, how do you use this
-// don't use this
-class WidgetService : RemoteViewsService() {
-    override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory {
-        return WidgetViewsFactory(this.applicationContext, "temperature")
-    }
-}
-
-// really doesn't work
-class WidgetViewsFactory(private val context: Context, private val dataType: String) : RemoteViewsService.RemoteViewsFactory {
-    private lateinit var db: FirebaseFirestore
-    private lateinit var auth: FirebaseAuth
-    private var currentValue: String = "None"
-
-    override fun onCreate() {
-        Log.d(TAG, "viewfactory created")
-        val rv = RemoteViews(context.packageName, R.layout.stat_display_widget)
-        rv.setTextViewText(R.id.waterlevel_text, "hey fuck you")
-        db = Firebase.firestore
-        auth = Firebase.auth
-        auth.currentUser?.let {
-            val uid = it.uid
-            val docRef = db.collection("ESP32data").document(uid).collection("sensorData")
-            docRef.orderBy("timestamp").limit(1).addSnapshotListener {
-                snapshot: QuerySnapshot?, error: FirebaseFirestoreException? ->
-                if (error != null) {
-                    Log.w(TAG, "Listen failed.", error)
-                    return@addSnapshotListener
-                }
-                if (snapshot != null && !snapshot.isEmpty) {
-                    Log.d(TAG, "Current data: ${snapshot.first().data}")
-                    currentValue = snapshot.first().data[dataType].toString()
-                    val appWidgetManager: AppWidgetManager = AppWidgetManager.getInstance(context)
-                    val appWidgetIds: IntArray = appWidgetManager.getAppWidgetIds(ComponentName(context.packageName, StatDisplayWidget::class.java.name))
-                    appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.waterlevel_text)
-                } else {
-                    Log.d(TAG, "No data in snapshot.")
-                }
-            }
-        }
-    }
-
-    override fun getLoadingView(): RemoteViews? {
-        return null
-    }
-
-    override fun getItemId(p0: Int): Long {
-        return p0.toLong()
-    }
-
-    override fun onDataSetChanged() {
-        Log.d(TAG, "onDataSetChanged called")
-    }
-
-    override fun hasStableIds(): Boolean {
-        return true
-    }
-
-    override fun getViewAt(p0: Int): RemoteViews {
-        Log.d(TAG, "attempt to get view at $p0")
-        val rv = RemoteViews(context.packageName, R.layout.stat_display_widget)
-        rv.setTextViewText(R.id.waterlevel_text, "$dataType: $currentValue")
-        return rv
-    }
-
-    override fun getCount(): Int {
-        return 1
-    }
-
-    override fun getViewTypeCount(): Int {
-        return 1
-    }
-
-    override fun onDestroy() {
-
     }
 }
