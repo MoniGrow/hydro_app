6860fbbaadc7d47c557ca3498b1a006fb3b545a7
Jerry Yan <jeyan@ucsd.edu>
2021-05-20 21:18:30 -0700

    Edit plant searching functional

--- lib/plant_monitor/cell_popup.dart
+++ lib/plant_monitor/cell_popup.dart
@@ -92,7 +92,8 @@ class _CellPopupState extends State<CellPopup> {
               onPressed: () => Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (_) => PlantEditor(plantData, widget.cell),
+                    builder: (_) =>
+                        PlantEditor(plantData, widget.cell, retrievePlant),
                   ))),
         ),
       ],
--- lib/plant_monitor/plant_editor.dart
+++ lib/plant_monitor/plant_editor.dart
@@ -8,8 +8,9 @@ import 'package:hydro_app/utils.dart';
 class PlantEditor extends StatefulWidget {
   final Map<String, dynamic> preExisting;
   final int cell;
+  final void Function() updateOnConfirm;
 
-  PlantEditor(this.preExisting, this.cell);
+  PlantEditor(this.preExisting, this.cell, this.updateOnConfirm);
 
   @override
   _PlantEditorState createState() => _PlantEditorState();
@@ -21,6 +22,7 @@ class _PlantEditorState extends State<PlantEditor> {
   String _hour = "00";
   String _minute = "00";
   String _time = "00:00";
+  String img_url = "";
 
   DateTime selectedDate = DateTime.now();
   TimeOfDay selectedTime = TimeOfDay(hour: 00, minute: 00);
@@ -28,6 +30,32 @@ class _PlantEditorState extends State<PlantEditor> {
   TextEditingController _nameController = TextEditingController();
   TextEditingController _dateController = TextEditingController();
   TextEditingController _timeController = TextEditingController();
+
+  void setFromPlantData(Map<dynamic, dynamic> data) {
+    _nameController.text = data["name"];
+    img_url = data["image_url"];
+  }
+
+  void setTimeFields(DateTime time) {
+    selectedDate = time;
+    selectedTime = TimeOfDay.fromDateTime(time);
+    _dateController.text = "${selectedDate.toLocal()}".split(' ')[0];
+    _hour = selectedTime.hour.toString();
+    _minute = selectedTime.minute.toString();
+    _time = _hour.padLeft(2, "0") + ' : ' + _minute.padLeft(2, "0");
+    _timeController.text = _time;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.preExisting != null) {
+      DateTime timestamp =
+          Utils.dateTimeFromSeconds(widget.preExisting["time_planted"]);
+      setTimeFields(timestamp);
+      _nameController.text = widget.preExisting["plant_name"];
+    }
+  }
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime picked = await showDatePicker(
@@ -47,7 +75,7 @@ class _PlantEditorState extends State<PlantEditor> {
       context: context,
       initialTime: selectedTime,
     );
-    if (picked != null)
+    if (picked != null) {
       setState(() {
         selectedTime = picked;
         _hour = selectedTime.hour.toString();
@@ -55,6 +83,7 @@ class _PlantEditorState extends State<PlantEditor> {
         _time = _hour.padLeft(2, "0") + ' : ' + _minute.padLeft(2, "0");
         _timeController.text = _time;
       });
+    }
   }
 
   @override
@@ -63,6 +92,17 @@ class _PlantEditorState extends State<PlantEditor> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Add new plant"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: PlantSearchDelegate(setFromPlantData),
+              );
+            },
+          ),
+        ],
       ),
       body: Form(
         key: _formKey,
@@ -72,14 +112,10 @@ class _PlantEditorState extends State<PlantEditor> {
               margin: EdgeInsets.symmetric(horizontal: 5),
               child: Column(
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "Search plant (does nothing)",
-                    ),
-                  ),
                   Row(
                     children: [
                       Text("Plant name"),
+                      Container(width: 5),
                       Expanded(
                         child: TextFormField(
                           validator: (value) {
@@ -139,6 +175,7 @@ class _PlantEditorState extends State<PlantEditor> {
                     var data = {
                       "cell${widget.cell}": {
                         "cell_num": widget.cell,
+                        "image_url": img_url,
                         "plant_name": plantName,
                         "time_planted":
                             parsedTime.millisecondsSinceEpoch.toDouble() / 1000
@@ -149,6 +186,7 @@ class _PlantEditorState extends State<PlantEditor> {
                         .reference()
                         .child("users/$uid/grown_plants")
                         .update(data);
+                    widget.updateOnConfirm();
                     Navigator.pop(context);
                   }
                 },
@@ -158,5 +196,112 @@ class _PlantEditorState extends State<PlantEditor> {
         ),
       ),
     );
+  }
+}
+
+class PlantSearchDelegate extends SearchDelegate {
+  List<Map<dynamic, dynamic>> plants = [];
+  final void Function(Map) selectedFunc;
+
+  PlantSearchDelegate(this.selectedFunc) {
+    loadPlants();
+  }
+
+  void loadPlants() async {
+    DatabaseReference ref =
+        FirebaseDatabase.instance.reference().child("plant_data");
+    await ref.once().then((snapshot) {
+      if (snapshot.value != null) {
+        Map<dynamic, dynamic>.from(snapshot.value).forEach((_, value) {
+          plants.add(value);
+        });
+      } else {
+        print("No data from plants loaded. Something went wrong.");
+      }
+      print(plants);
+    });
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  Widget buildSearchColumn() {
+    if (query.length == 0) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Text(
+              "Type a name of a plant!",
+            ),
+          )
+        ],
+      );
+    }
+
+    if (plants.isEmpty) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(child: CircularProgressIndicator()),
+        ],
+      );
+    }
+
+    var filtered = plants.where((plant) =>
+        plant["name"].toString().toLowerCase().contains(query.toLowerCase()));
+    if (filtered.length == 0) {
+      return Column(
+        children: <Widget>[
+          Text(
+            "No Results Found.",
+          ),
+        ],
+      );
+    }
+    var filteredList = filtered.toList();
+    return ListView.builder(
+      itemCount: filtered.length,
+      itemBuilder: (context, index) {
+        var result = filteredList[index]["name"];
+        return ListTile(
+          title: Text(result),
+          onTap: () {
+            selectedFunc(filteredList[index]);
+            close(context, null);
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return buildSearchColumn();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return buildSearchColumn();
   }
 }
